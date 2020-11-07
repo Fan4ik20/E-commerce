8b8afd81eb9733a94b018467fe061b0746ae07bc
Fan4ik20 <61667639+Fan4ik20@users.noreply.github.com>
2020-11-07 15:24:05 +0100

    For Cart Added templates and models

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,5 +1,8 @@
 from .models import Category
 
+# Context processor - ф-ція, яка приймає один аргумент й повертає словник
+# Повернений словник буде доступним в любому місці коду.
+
 
 def menu_links(request):
     links = Category.objects.all()
